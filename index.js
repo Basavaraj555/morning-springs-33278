ba68ff985264ef4a7e7e9b8b64262c9f09f89e09
Basavaraj555 <53262156+Basavaraj555@users.noreply.github.com>
2021-04-06 01:13:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const connectDB = require("./config/db");
-const path=require("path");
-const cors=require("cors");
+const cors = require("cors");
+
 
 connectDB();
 
 app.use(express.json({ extended: false }));
+
 //cors access to the server
 app.use(cors());
+
+// app.use(function(req, res, next) {
 //   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 //   res.setHeader("Access-Control-Allow-Methods", "*");
 //   res.setHeader("Access-Control-Allow-Headers", "*");
+
 //   next();
 // });
 
 // app.get("/", (req, res) => {
-//   res.send("Welcome to the Express-mongoose-node application ");
+//   res.send("welcome to express-mongoose node application");
 // });
 
-app.use("/api/students", require("./routes/api/students"));
+app.use("/api/students", cors(), require("./routes/api/students"));
+//app.use("/api/teachers", require("./routes/api/teacher"));
 
-if( process.env.NODE_ENV==="production"){
+//setting to be made before uploading to heroku
+
+if (process.env.NODE_ENV === "production") {
+  //set static folder
   app.use(express.static("client/build"));
-  app.get("*", (req,res)=>{
-   res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`server is running at port ${port}`);
+  console.log(`Server is running at port ${port}`);
 });
